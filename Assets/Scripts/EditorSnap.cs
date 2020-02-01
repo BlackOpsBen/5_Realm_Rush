b37bffce25a983fba3552f613946ebf49087354e
Ben Mora <blackopsben@gmail.com>
2020-01-31 18:14:49 -0600

    Cube and snapping done. Ready for 124

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1.0f, 20.0f)][SerializeField] float gridSize = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+    }
+}
